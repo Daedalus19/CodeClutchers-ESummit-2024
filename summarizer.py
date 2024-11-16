761ebd1bd2b24d67a4123e2ac43e1f94514d50ec
Daedalus19 <realsusmit18@gmail.com>
2024-11-16 22:11:56 +0530

    summariser done

--- summarizer.py
+++ summarizer.py
@@ -1,3 +1,10 @@
+
+'''
+    using Pegasus model from Hugging face
+'''
+
+
+# its slow
 from transformers import pipeline
 
 pipe = pipeline("summarization", model="google/pegasus-large")
@@ -15,3 +22,28 @@ context_chunks = chunk_text(context)
 for text in context_chunks:
     print(pipe(text))
 
+
+
+'''
+    Using ollama llama3.2 and langchain  
+'''
+
+# Make a 2nd model using llama to summarise text
+
+from langchain_ollama import OllamaLLM
+
+def summary():
+    model = OllamaLLM(model="llama3.2")
+
+    with open("assets\\transcribe.txt") as file :
+        prompt = file.read()
+
+    prompt = "Summarise this : " + prompt 
+    result = model.invoke(input=prompt)
+    
+    with open("assets\\summary.txt","w") as fileObject :
+        fileObject.write(result)
+
+if __name__ == "__main__":
+    summary()
+    
